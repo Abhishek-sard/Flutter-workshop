1d02659fc4bc0378baf7eba87297174ff2d2ea99
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-09-24 14:24:59 +0545

    push

--- training/lib/main.dart
+++ training/lib/main.dart
@@ -9,9 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Container Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: ContainerPage(),
       debugShowCheckedModeBanner: false,
     );
@@ -22,56 +20,55 @@ class ContainerPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("App Bar")),
+      appBar: AppBar(title: Text("cart page")),
       backgroundColor: Colors.green[200],
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Cart with text
             Container(
-              height: 100,
-              width: double.infinity,
+              height: 120,
+              width: 500,
               decoration: BoxDecoration(
-                color: Colors.red,
+                color: Colors.orange,
                 borderRadius: BorderRadius.circular(12),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6,
+                    offset: Offset(2, 2),
+                  ),
+                ],
               ),
-              child: Center(
-                child: Icon(Icons.home, color: Colors.white, size: 40),
-              ),
-            ),
-            SizedBox(height: 16),
-            Container(
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.green,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Center(
-                child: Icon(Icons.favorite, color: Colors.white, size: 40),
-              ),
-            ),
-            SizedBox(height: 16),
-            Padding(
-              padding: const EdgeInsets.all(100, 0),
-              child: Image.asset(
-                "assets/logo3.jpg",
-                height: 200,
-                width: 200,
-                fit: BoxFit.cover,
-              ),
-
-            )
-            Container(
-              height: 100,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: Colors.lightGreen,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Center(
-                child: Text("k x ani bhujdae xau"),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(
+                  children: [
+                    Icon(Icons.shopping_cart, color: Colors.white, size: 40),
+                    SizedBox(width: 16),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Cart Item",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        Text(
+                          "This is your cart description",
+                          style: TextStyle(fontSize: 16, color: Colors.white70),
+                        ),
+                        Text("Price: \$100"),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
